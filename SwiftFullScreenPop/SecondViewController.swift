c487a16f870e46222e525d719b5c2cc83c0d2781
liping <liping@qfang.com>
2017-06-21 11:13:15 +0800

    Initial Commit

---
+++ SwiftFullScreenPop/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SwiftFullScreenPop
+//
+//  Created by QFWangLP on 2017/6/21.
+//  Copyright © 2017年 leefenghy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
